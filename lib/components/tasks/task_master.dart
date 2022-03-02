8aca8192d7e218f30b3fa8bcbc8400f16de7887a
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-03-02 15:18:44 +0100

    feat(app): add minimal code on components

---
+++ lib/components/tasks/task_master.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskMaster extends StatefulWidget {
+  const TaskMaster({Key? key}) : super(key: key);
+
+  @override
+  State<TaskMaster> createState() => _TaskMasterState();
+}
+
+class _TaskMasterState extends State<TaskMaster> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
